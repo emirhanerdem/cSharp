5cd558e8d9b060481edfae188c5fac42647de729
emirhan <116457536+emirhanerdem@users.noreply.github.com>
2025-04-10 16:21:10 +0300

    Add files via upload

---
+++ Musteri.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HaliYikamaTakipSistemi
+{
+    public class Musteri
+    {
+        public string Ad { get; set; }
+        public string Soyad { get; set; }
+        public string Telefon { get; set; }
+        public string Adres { get; set; }
+
+        public List<Hali> Halilar { get; set; } = new List<Hali>();
+
+        public string TamAd => $"{Ad} {Soyad}";
+        public override string ToString()
+        {
+            return TamAd;
+        }
+    }
+}
